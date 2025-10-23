9553e63ba39b66ae41602ce53eda7224eaf75e14
AmirTaha <131557921+AmirTahaF@users.noreply.github.com>
2025-10-23 16:54:34 +0330

    Update binary_search.py
    Here you can see a simple code form of binary search for a list of number in python

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,8 @@
+# For any list of n items, binary search will take log base 2 of n steps to run in the worst case
+# we have to find the middle element and then compare it to our target number to check that it is bigger or smaller 
+
 # Binary search 
 import random 
-
 
 # 1 to 100 
 numList = [n for n in range(1,101)]
@@ -8,12 +10,9 @@ numList = [n for n in range(1,101)]
 # to Choose a Random number 
 targetNum = random.choice(numList)
 
-
 def binary_search (lis , item):
-
     firstIndex = 0 
-    lastIndex = len(lis) - 1 
-    
+    lastIndex = len(lis) - 1  
     operationCount = 0 
 
     while (firstIndex <= lastIndex):
@@ -28,5 +27,6 @@ def binary_search (lis , item):
             lastIndex = mid -1 
     # return none if item is not existed 
     return None
+
 
 print(binary_search(numList,targetNum))
