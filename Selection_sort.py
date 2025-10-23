15a9d31c35308ed9c26e14f7dbd53de123360088
AmirTaha <131557921+AmirTahaF@users.noreply.github.com>
2025-10-23 15:14:10 +0330

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,3 +1,11 @@
+# first of all we have to find the smallest number in a list and the append it to our new list
+# For Bog O :
+# you don’t have to check a list of n elements each time.
+# You check n elements, then n – 1, n - 2 … 2, 1. On average, you check a
+# list that has 1/2 × n elements. The runtime is O(n × 1/2 × n).
+# But constants like 1/2 are ignored in Big O notation
+# so ==> O(n**2)
+
 listOfNum = [30,60,33,45,87,13,9,56,100,34,77,69,20,2]
 
 def find_smallest(arr):
@@ -20,3 +28,4 @@ def selection_sort(arr):
 
 print(selection_sort(listOfNum))
 # output => [2, 9, 13, 20, 30, 33, 34, 45, 56, 60, 69, 77, 87, 100] 
+
